13258cce0d23e40881f65c37971052c9448e18db
Scotty Franzyshen <sfranzyshen@gmail.com>
2020-08-16 16:19:52 -0700

    Update servo.py
    expand to support arduino style functions ... see remarks st top of file ...

--- servo.py
+++ servo.py
@@ -1,6 +1,42 @@
+"""
+  The methods are:
+    Servo - Class for manipulating servo motors connected to pins.
+    attach(pin )  - Attaches a servo motor to an i/o pin.
+    attach(pin, min, max  ) - Attaches to a pin setting min and max values in microseconds
+    default min is 544, max is 2400  
+ 
+    write()     - Sets the servo angle in degrees.  (invalid angle that is valid as pulse in microseconds is treated as microseconds)
+    writeMicroseconds() - Sets the servo pulse width in microseconds 
+    read()      - Gets the last written servo pulse width as an angle between 0 and 180. 
+    readMicroseconds()   - Gets the last written servo pulse width in microseconds. (was read_us() in first release)
+    attached()  - Returns true if there is a servo attached. 
+    detach()    - Stops an attached servos from pulsing its i/o pin. 
+    
+    class Servo
+{
+public:
+    Servo();
+    ~Servo();
+    uint8_t attach(int pin);           // attach the given pin to the next free channel, sets pinMode, returns channel number or 0 if failure
+    uint8_t attach(int pin, uint16_t min, uint16_t max); // as above but also sets min and max values for writes. 
+    void detach();
+    void write(int value);             // if value is < 200 its treated as an angle, otherwise as pulse width in microseconds 
+    void writeMicroseconds(int value); // Write pulse width in microseconds 
+    int read();                        // returns current pulse width as an angle between 0 and 180 degrees
+    int readMicroseconds();            // returns current pulse width in microseconds for this servo (was read_us() in first release)
+    bool attached();                   // return true if this servo is attached, otherwise false 
+private:
+    static uint32_t _servoMap;
+    bool     _attached;
+    uint8_t  _pin;
+    uint16_t _minUs;                   
+    uint16_t _maxUs;                   
+    uint16_t _valueUs;
+};
+"""
+
 from machine import PWM
 import math
-
 
 class Servo:
     """
