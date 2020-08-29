f84b2d30907dc273b2a63c0cb9826c6ba2fc0187
Scotty Franzyshen <sfranzyshen@gmail.com>
2020-08-29 14:03:44 -0700

    Update servo.py
    clean up and expand to match arduino servo driver behavior

--- servo.py
+++ servo.py
@@ -1,63 +1,29 @@
-"""
-  The methods are:
-    Servo - Class for manipulating servo motors connected to pins.
-    attach(pin )  - Attaches a servo motor to an i/o pin.
-    attach(pin, min, max  ) - Attaches to a pin setting min and max values in microseconds
-    default min is 544, max is 2400  
- 
-    write()     - Sets the servo angle in degrees.  (invalid angle that is valid as pulse in microseconds is treated as microseconds)
-    writeMicroseconds() - Sets the servo pulse width in microseconds 
-    read()      - Gets the last written servo pulse width as an angle between 0 and 180. 
-    readMicroseconds()   - Gets the last written servo pulse width in microseconds. (was read_us() in first release)
-    attached()  - Returns true if there is a servo attached. 
-    detach()    - Stops an attached servos from pulsing its i/o pin. 
-    
-    class Servo
-{
-public:
-    Servo();
-    ~Servo();
-    uint8_t attach(int pin);           // attach the given pin to the next free channel, sets pinMode, returns channel number or 0 if failure
-    uint8_t attach(int pin, uint16_t min, uint16_t max); // as above but also sets min and max values for writes. 
-    void detach();
-    void write(int value);             // if value is < 200 its treated as an angle, otherwise as pulse width in microseconds 
-    void writeMicroseconds(int value); // Write pulse width in microseconds 
-    int read();                        // returns current pulse width as an angle between 0 and 180 degrees
-    int readMicroseconds();            // returns current pulse width in microseconds for this servo (was read_us() in first release)
-    bool attached();                   // return true if this servo is attached, otherwise false 
-private:
-    static uint32_t _servoMap;
-    bool     _attached;
-    uint8_t  _pin;
-    uint16_t _minUs;                   
-    uint16_t _maxUs;                   
-    uint16_t _valueUs;
-};
-"""
-
-from machine import PWM
-import math
+import machine, math
 
 class Servo:
     """
-    A simple class for controlling hobby servos.
-
-    Args:
-        pin (machine.Pin): The pin where servo is connected. Must support PWM.
-        freq (int): The frequency of the signal, in hertz.
-        min_us (int): The minimum signal length supported by the servo.
-        max_us (int): The maximum signal length supported by the servo.
-        angle (int): The angle between the minimum and maximum positions.
-
+    A simple class for controlling hobby servos. Modeled after the ESP8266 Arduino Servo Driver
     """
-    def __init__(self, pin, freq=50, min_us=600, max_us=2400, angle=180):
+    def __init__(self, pin, freq=50, min_us=24, max_us=115, max_ang=180):
         self.min_us = min_us
         self.max_us = max_us
-        self.us = 0
         self.freq = freq
-        self.angle = angle
-        self.pwm = PWM(pin, freq=freq, duty=0)
+        self.pin = machine.Pin(pin)
+        self.max_ang = max_ang
+        self.pwm = None
+        self._attached = False
+        
+    def attach(self, pin=self.pin, freq=self.freq):
+        self.pwm = machine.PWM(pin, freq=freq, duty=0)
+        self._attached = True
+        
+    def detach(self):
+        self.pwm.deinit()
+        self._attached = False
 
+    def attached(self):
+        return(self._attached)
+      
     def write_us(self, us):
         """Set the signal to be ``us`` microseconds long. Zero disables it."""
         if us == 0:
@@ -67,11 +33,12 @@ class Servo:
         duty = us * 1024 * self.freq // 1000000
         self.pwm.duty(duty)
 
-    def write_angle(self, degrees=None, radians=None):
-        """Move to the specified angle in ``degrees`` or ``radians``."""
-        if degrees is None:
-            degrees = math.degrees(radians)
+    def write(self, degrees):
+        """Move to the specified angle in ``degrees``."""
         degrees = degrees % 360
         total_range = self.max_us - self.min_us
-        us = self.min_us + total_range * degrees // self.angle
+        us = self.min_us + total_range * degrees // self.max_ang
         self.write_us(us)
+        
+    def __deinit__(self):
+        self.pwm.deinit()
