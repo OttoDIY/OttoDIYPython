33015dd0ce462548c169efe0c6e7b6375e32066a
Scotty Franzyshen <sfranzyshen@gmail.com>
2020-08-29 20:26:31 -0700

    Update oscillator.py

--- oscillator.py
+++ oscillator.py
@@ -1,71 +1,38 @@
 """
-class Oscillator
-{
-  public:
-    Oscillator(int trim=0) {_trim=trim;};
-    void attach(int pin, bool rev =false);
-    void detach();
-    
-    void SetA(unsigned int A) {_A=A;};
-    void SetO(unsigned int O) {_O=O;};
-    void SetPh(double Ph) {_phase0=Ph;};
-    void SetT(unsigned int T);
-    void SetTrim(int trim){_trim=trim;};
-    int getTrim() {return _trim;};
-    void SetPosition(int position); 
-    void Stop() {_stop=true;};
-    void Play() {_stop=false;};
-    void Reset() {_phase=0;};
-    void refresh();
-    
-  private:
-    bool next_sample();  
-    
-  private:
-    //-- Servo that is attached to the oscillator
-    Servo _servo;
-    
-    //-- Oscillators parameters
-    unsigned int _A;  //-- Amplitude (degrees)
-    unsigned int _O;  //-- Offset (degrees)
-    unsigned int _T;  //-- Period (miliseconds)
-    double _phase0;   //-- Phase (radians)
-    
-    //-- Internal variables
-    int _pos;         //-- Current servo pos
-    int _trim;        //-- Calibration offset
-    double _phase;    //-- Current phase
-    double _inc;      //-- Increment of phase
-    double _N;        //-- Number of samples
-    unsigned int _TS; //-- sampling period (ms)
-    
-    long _previousMillis; 
-    long _currentMillis;
-    
-    //-- Oscillation mode. If true, the servo is stopped
-    bool _stop;
-
-    //-- Reverse mode
-    bool _rev;
-};
-
 MicroPython driver to Generate sinusoidal oscillations in the servos
 Requires a Pulse width modulation (PWM) pin. On the ESP8266 the 
-pins 0, 2, 4, 5, 12, 13, 14 and 15 all support PWM. 
+pins 0, 2, 4, 5, 12, 13, 14 and 15 are all support PWM. 
 The limitation is that they must all be at the same frequency, 
 and the frequency must be between 1Hz and 1kHz.
 Oscillator.pde: GPL license (c) Juan Gonzalez-Gomez (Obijuan), 2011
+OttDIY Python Project, 2020 
 """
 import math, time, servo
 
 Class Oscillator:
 	def __init__(self, trim = 0):
-		self._trim = trim
-		FIXME
-		
+		# Oscillators parameters
+		self._A = 0		# Amplitude (degrees)
+		self._O = 0		# Offset (degrees)
+		self._T = 0		# Period (miliseconds)
+		self._phase0 = 0.0	# Phase (radians)
+
+		# Internal variables
+		self._servo = servo.Servo() # Servo that is attached to the oscillator
+		self._pos = 0		# Current servo pos
+		self._trim = trim	# Calibration offset
+		self._phase = 0.0	# Current phase
+		self._inc = 0.0		# Increment of phase
+		self._N = 0.0		# Number of samples
+		self._TS = 0		# sampling period (ms)
+		self._previousMillis = 0 
+		self._currentMillis = 0
+		self._stop = True	# Oscillation mode. If true, the servo is stopped
+		self._rev = False	# Reverse mode
+
 	#-- Attach an oscillator to a servo
-	#-- Input: pin is the arduino pin were the servo is connected
-	def attach(self, pin, rev): 
+	#-- Input: pin is the pin were the servo is connected
+	def attach(self, pin, rev = False): 
 		if not self._servo.attached():	#-- If the oscillator is detached,
 			self._servo.attach(pin)	#-- Attach the servo and move it to the home position
 			self._servo.write(90)
@@ -90,7 +57,7 @@ Class Oscillator:
 	#-- Detach an oscillator from his servo
 	def detach(self):
 		if self._servo.attached(): #-- If the oscillator is attached,
-		self._servo.detach()
+			self._servo.detach()
 
 	#--  Set the oscillator Phase (radians)
 	def SetA(self, int A):
@@ -134,29 +101,29 @@ Class Oscillator:
 	def Reset(self):
 		self._phase = 0
 
+	#-- should be taken (i.e. the TS time has passed since
+	#-- the last sample was taken
+	def __next_sample(self):
+		self._currentMillis = time.ticks_ms() #-- Read current time
+		if self._currentMillis - self._previousMillis > self._TS:		
+			self._previousMillis = self._currentMillis;   
+			return True
+		return False
+
 	"""
 	This function should be periodically called
 	in order to maintain the oscillations. It calculates
 	if another sample should be taken and position the servo if so
 	"""
 	def refresh(self):
-		if next_sample(): #-- Only When TS milliseconds have passed, sample is obtained
+		if __next_sample(): #-- Only When TS milliseconds have passed, sample is obtained
 			if not self._stop: #-- If the oscillator is not stopped, the servo position
 				self._pos = math.round(self._A * math.sin(self._phase + self._phase0) + self._O) #-- Sample the sine function and set the servo pos
-			if self._rev: 
-				self._pos = -self._pos FIXME
-			self._servo.write(self._pos + 90 + self._trim)
+				if self._rev: 
+					self._pos = -self._pos
+				self._servo.write(self._pos + 90 + self._trim)
 
 			#-- Increment the phase
 			#-- It is always increased, when the oscillator is stop
 			#-- so that the coordination is always kept
 			self._phase = self._phase + self._inc
-
-#-- should be taken (i.e. the TS time has passed since
-#-- the last sample was taken
-def next_sample(self):
-	self._currentMillis = time.ticks_ms() #-- Read current time
-	if self._currentMillis - self._previousMillis > self._TS:		
-		self._previousMillis = self._currentMillis;   
-		return True
-	return False
