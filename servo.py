03106253b22fc8ffb7cda7bf29689b9088bb1c41
Scotty Franzyshen <sfranzyshen@gmail.com>
2020-08-29 20:09:04 -0700

    Update servo.py

--- servo.py
+++ servo.py
@@ -1,11 +1,10 @@
-
+"""
+A simple class for controlling hobby servos. Modeled after the ESP8266 Arduino Servo Driver
+OttDIY Python Project, 2020
+"""
 import machine
 
 class Servo:
-    """
-    A simple class for controlling hobby servos. Modeled after the ESP8266 Arduino Servo Driver
-    OttDIY Python Project, 2020
-    """
     def __init__(self, freq=50, min_us=24, max_us=115, max_ang=180):
         self.min_us = min_us
         self.max_us = max_us
@@ -14,19 +13,19 @@ class Servo:
         self.pin = None
         self.pwm = None
         self._attached = False
-        
+
     def attach(self, pin):
         self.pin = machine.Pin(pin)
         self.pwm = machine.PWM(self.pin, freq=self.freq, duty=0)
         self._attached = True
-        
+
     def detach(self):
         self.pwm.deinit()
         self._attached = False
 
     def attached(self):
         return(self._attached)
-      
+
     def write_us(self, us):
         """Set the signal to be ``us`` microseconds long. Zero disables it."""
         self.pwm.duty(us)
@@ -37,7 +36,7 @@ class Servo:
         total_range = self.max_us - self.min_us
         us = self.min_us + total_range * degrees // self.max_ang
         self.write_us(us)
-        
+
     def __deinit__(self):
         self.pwm.deinit()
 
