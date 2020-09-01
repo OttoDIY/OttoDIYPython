5e916118c36d9c6cbd637a600aa4d425e9bb16f7
Scotty Franzyshen <sfranzyshen@gmail.com>
2020-09-01 14:47:14 -0700

    Update otto9.py
    uPyCraft can not upload files with "..."

--- otto9.py
+++ otto9.py
@@ -36,7 +36,7 @@ class Otto9:
 		self.setRestState(False)
 		if load_calibration == True:
 			for i in range(0, 5):
-				#servo_trim = EEPROM.read(i) #-- FIXME ... add some sort of eeprom emulation
+				#servo_trim = EEPROM.read(i) #-- FIXME add some sort of eeprom emulation
 				servo_trim = 0
 				if servo_trim > 128:
 					servo_trim -= 256
@@ -64,7 +64,7 @@ class Otto9:
 
 	def saveTrimsOnEEPROM(self):
 		for i in range(0, 5):
-			#EEPROM.write(i, self._servo[i].getTrim()) #-- FIXME  ... add some sort of eeprom emulation
+			#EEPROM.write(i, self._servo[i].getTrim()) #-- FIXME add some sort of eeprom emulation
 			pass
 
 	#-- BASIC MOTION FUNCTIONS
