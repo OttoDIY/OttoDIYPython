5702260ae5e9a396068976eec9b29762e5bdc874
Scotty Franzyshen <sfranzyshen@gmail.com>
2020-09-23 15:27:41 -0700

    Update gestures.py

--- gestures.py
+++ gestures.py
@@ -2,16 +2,18 @@
 #-- gestures supported by Otto
 #-- OttDIY Python Project, 2020
 
-OTTOHAPPY      = 0
-OTTOSUPERHAPPY = 1
-OTTOSAD        = 2
-OTTOSLEEPING   = 3
-OTTOFART       = 4
-OTTOCONFUSED   = 5
-OTTOLOVE       = 6
-OTTOANGRY      = 7
-OTTOFRETFUL    = 8
-OTTOMAGIC      = 9
-OTTOWAVE       = 10
-OTTOVICTORY    = 11
-OTTOFAIL       = 12
+from micropython import const
+
+OTTOHAPPY      = const(0)
+OTTOSUPERHAPPY = const(1)
+OTTOSAD        = const(2)
+OTTOSLEEPING   = const(3)
+OTTOFART       = const(4)
+OTTOCONFUSED   = const(5)
+OTTOLOVE       = const(6)
+OTTOANGRY      = const(7)
+OTTOFRETFUL    = const(8)
+OTTOMAGIC      = const(9)
+OTTOWAVE       = const(10)
+OTTOVICTORY    = const(11)
+OTTOFAIL       = const(12)
