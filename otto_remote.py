36c921bec0a72b11f0d9a730f6b61f0d5329d1b4
Michael Weiss <github@mishafarms.us>
2021-01-01 09:37:27 -0800

    Update so that websockets will be easier to support

--- otto_remote.py
+++ otto_remote.py
@@ -12,17 +12,17 @@ import utime
 
 
 class ottoRemote(otto9.Otto9):
-    def __init__(self, name = "Otto", prgId = "Otto_V9"):
+    def __init__(self, name="Otto", prgId="Otto_V9"):
         super().__init__()
         self.timer = Timer(0)
         self.pending = False
-        self.moveList = []
-        self.savedMove = None
+        self.cmdList = []
+        self.savedCmd = None
         self.name = name
-        self.rfComm = RFCOMM(device = self.name, server = "ESP32")
+        self.rfComm = RFCOMM(device=self.name, server="ESP32")
         self.rfComm.callback(RFCOMM.CBTYPE_PATTERN, self.rfRemoteCommand, pattern='\r')
-        self.commandDebug = False
-        self.printMove = False
+        self.cmdDebug = False
+        self.printCmd = False
         self.moveDebug = False
         self.prgId = prgId
 
@@ -58,29 +58,33 @@ class ottoRemote(otto9.Otto9):
         self.rfComm.deinit()
         super().deinit()
 
-    def moveFunc(self, _):
+    def cmdProc(self, _):
         self.pending = False
-        move = None
-        if len(self.moveList) > 0:
+
+        if len(self.cmdList) > 0:
             # make otto move
-            move = self.moveList.pop(0).split()
-        elif self.savedMove is not None:
-            move = self.savedMove
+            cmd = self.cmdList.pop(0)
+        elif self.savedCmd is not None:
+            cmd = self.savedCmd
+        else:
+            return
 
-        self.savedMove = None
+        command = cmd['command']
 
-        if move[0] == 'M':
-            self.sendAck()
+        self.savedCmd = None
+
+        if command[0] == 'M':
+            self.sendAck(cmd)
             # 'M' is for move
-            moveIndex = int(move[1])
+            moveIndex = int(command[1])
 
-            if len(move) > 2:
-                moveTime = int(move[2])
+            if len(command) > 2:
+                moveTime = int(command[2])
             else:
                 moveTime = 1000
 
-            if len(move) > 3:
-                moveSize = int(move[3])
+            if len(command) > 3:
+                moveSize = int(command[3])
             else:
                 moveSize = 15
 
@@ -88,8 +92,9 @@ class ottoRemote(otto9.Otto9):
                 print("index = " + str(moveIndex) + " T = " + str(moveTime) + " moveSize = " + str(moveSize))
 
             if len(self.ottoMoves) > moveIndex:
+                # this should use the otto.getRestState()
                 if moveIndex != 0:
-                    self.savedMove = move
+                    self.savedCmd = cmd
 
                 # get the actual move
                 actualMove = self.ottoMoves[moveIndex]
@@ -98,139 +103,141 @@ class ottoRemote(otto9.Otto9):
                     # get the specific move
                     ottoMove = actualMove[0]
                     if len(actualMove) == 2:
-                        self.movePrint(ottoMove.__name__ + "(" + str(actualMove[1]) + ", " + str(moveTime) + ")")
+                        self.cmdPrint(ottoMove.__name__ + "(" + str(actualMove[1]) + ", " + str(moveTime) + ")")
                         ottoMove(actualMove[1], moveTime)
                     elif len(actualMove) == 3:
                         if actualMove[2] == 0:
-                            self.movePrint(ottoMove.__name__ + "(" + str(actualMove[1]) + ", " +
-                                           str(moveTime) + ", " + str(moveSize) + ")")
+                            self.cmdPrint(ottoMove.__name__ + "(" + str(actualMove[1]) + ", " +
+                                          str(moveTime) + ", " + str(moveSize) + ")")
                             ottoMove(actualMove[1], moveTime, moveSize)
                         elif actualMove[2] >= -1:
-                            self.movePrint(ottoMove.__name__ + "(" + str(actualMove[1]) + ", " +
-                                           str(moveTime) + ", " + str(actualMove[2]) + ")")
+                            self.cmdPrint(ottoMove.__name__ + "(" + str(actualMove[1]) + ", " +
+                                          str(moveTime) + ", " + str(actualMove[2]) + ")")
                             ottoMove(actualMove[1], moveTime, actualMove[2])
                         elif actualMove[2] < -1:
-                            self.movePrint(ottoMove.__name__ + "(" + str(actualMove[1]) + ")")
+                            self.cmdPrint(ottoMove.__name__ + "(" + str(actualMove[1]) + ")")
                             ottoMove(actualMove[1])
                         else:
-                            self.savedMove = None
+                            self.savedCmd = None
                     elif len(actualMove) == 4:
-                        self.movePrint(ottoMove.__name__ + "(" + str(actualMove[1]) + ", " + str(moveTime) +
-                                       ", " + str(moveSize) + ", " + str(actualMove[3]) + ")")
+                        self.cmdPrint(ottoMove.__name__ + "(" + str(actualMove[1]) + ", " + str(moveTime) +
+                                      ", " + str(moveSize) + ", " + str(actualMove[3]) + ")")
                         ottoMove(actualMove[1], moveTime, moveSize, actualMove[3])
                     else:
-                        self.savedMove = None
+                        self.savedCmd = None
                 else:
                     ottoMove = actualMove
-                    self.movePrint(ottoMove.__name__ + "()")
+                    self.cmdPrint(ottoMove.__name__ + "()")
                     ottoMove()
 
-                self.sendFinalAck()
-        elif move[0] == 'B':
+                self.sendFinalAck(cmd)
+        elif command[0] == 'B':
             # 'B' is for request battery
             print("Otto request battery")
-            batteryLevel = 0.0
             batteryLevel = super().getBatteryLevel()
 
-            if self.rfComm .connected()[0] != 0:
-                self.rfComm.write(0, "&&B " + str(batteryLevel) + "%%\r")
-        elif move[0] == 'D':
+            self.sendResp(cmd, str(batteryLevel))
+        elif command[0] == 'D':
             # 'D' is for request distance
             print("Otto request distance")
-            distance = 400.0
             distance = super().getDistance()
 
-            if self.rfComm .connected()[0] != 0:
-                self.rfComm.write(0, "&&D " + str(distance) + "%%\r")
-        elif move[0] == 'H':
-            self.sendAck()
+            self.sendResp(cmd, str(distance))
+        elif command[0] == 'H':
+            self.sendAck(cmd)
             # 'H' is for gesture
-            gesture = int(move[1]) - 1
-            self.movePrint("Otto gesture=" + str(gesture))
+            gesture = int(command[1]) - 1
+            self.cmdPrint("Otto gesture=" + str(gesture))
             super().playGesture(gesture)
-            self.sendFinalAck()
-        elif move[0] == 'I':
+            self.sendFinalAck(cmd)
+        elif command[0] == 'I':
             # 'I' is for request program ID
             print("Otto request program ID")
-            if self.rfComm .connected()[0] != 0:
-                self.rfComm.write(0, "&&I " + self.prgId + "%%\r")
-        elif move[0] == 'K':
-            self.sendAck()
+            self.sendResp(cmd, self.prgId)
+        elif command[0] == 'K':
+            self.sendAck(cmd)
             # 'K' is for sing
-            song = int(move[1]) - 1
-            self.movePrint("Otto sing=" + str(song))
+            song = int(command[1]) - 1
+            self.cmdPrint("Otto sing=" + str(song))
             super().sing(song)
-            self.sendFinalAck()
-        elif move[0] == 'L':
-            self.sendAck()
+            self.sendFinalAck(cmd)
+        elif command[0] == 'L':
+            self.sendAck(cmd)
             # 'L' is for mouth
-            mouth = int(move[1], 2)
-            self.movePrint("Otto mouth=" + hex(mouth))
+            mouth = int(command[1], 2)
+            self.cmdPrint("Otto mouth=" + hex(mouth))
             super().putMouth(mouth, False)
-            self.sendFinalAck()
-        elif move[0] == 'N':
+            self.sendFinalAck(cmd)
+        elif command[0] == 'N':
             # 'N' is for request noise
             print("Otto request noise")
-            noise = 0
             noise = super().getNoise()
 
-            if self.rfComm .connected()[0] != 0:
-                self.rfComm.write(0, "&&N " + str(noise) + "%%\r")
-        elif move[0] == 'T':
-            self.sendAck()
+            self.sendResp(cmd, str(noise))
+        elif command[0] == 'T':
+            self.sendAck(cmd)
             # 'T' is for buzzer
 
-            if len(move) < 3:
+            if len(command) < 3:
                 # this is an error
                 super().putMouth(mouths.XMOUTH)
                 utime.sleep_ms(2000)
                 super().clearMouth()
             else:
-                self.movePrint("Otto buzzer freq=" + move[1] + " time=" + move[2])
-                super()._tone(int(move[1]), int(move[2]), 1)
-            self.sendFinalAck()
+                self.cmdPrint("Otto buzzer freq=" + command[1] + " time=" + command[2])
+                super()._tone(int(command[1]), int(command[2]), 1)
+            self.sendFinalAck(cmd)
         else:
-            self.sendAck()
+            self.sendAck(cmd)
             super().home()
-            self.sendFinalAck()
+            self.sendFinalAck(cmd)
 
-        # schedule again if the savedMove is valid
-        if self.savedMove is not None:
+        # schedule again if the savedCmd is valid
+        if self.savedCmd is not None:
             self.pending = True
-            self.timer.init(period=100, mode=Timer.ONE_SHOT, callback = self.moveFunc)
+            self.timer.init(period=100, mode=Timer.ONE_SHOT, callback=self.cmdProc)
 
-    def addMove(self, moveStr):
-        self.moveList.append(moveStr)
+    def addCmd(self, cmd):
+        self.cmdList.append(cmd)
         if not self.pending:
-            self.moveFunc(self.timer)
+            self.cmdProc(self.timer)
 
-    def sendAck(self):
+    def sendAck(self, cmd):
         utime.sleep_ms(30)
-        if self.rfComm.connected()[0] != 0:
-            self.rfComm.write(0, "&&A%%\r")
+        if cmd['src'] == 'RFCOMM':
+            if self.rfComm.connected()[cmd['ch']] != 0:
+                self.rfComm.write(cmd['ch'], "&&A%%\r")
 
-    def sendFinalAck(self):
+    def sendResp(self, cmd, result):
+        if cmd['src'] == 'RFCOMM':
+            if self.rfComm.connected()[cmd['ch']] != 0:
+                self.rfComm.write(cmd['ch'], '&&' + cmd['command'][0] + ' ' + result + '%%\r')
+
+    def sendFinalAck(self, cmd):
         utime.sleep_ms(30)
-        if self.rfComm.connected()[0] != 0:
-            self.rfComm.write(0, "&&F%%\r")
+        if cmd['src'] == 'RFCOMM':
+            if self.rfComm.connected()[cmd['ch']] != 0:
+                self.rfComm.write(cmd['ch'], "&&F%%\r")
 
     def rfRemoteCommand(self, ev):
-        # we don't care which channel
-        if self.commandDebug:
-            print(ev[2].decode())
-        command = ev[2].decode()
-        self.addMove(command)
+        cmd = {
+            'src': 'RFCOMM',
+            'ch': ev[0],
+            'command': ev[2].decode().split()
+        }
+        if self.cmdDebug:
+            print(cmd)
+        self.addCmd(cmd)
 
-    def commandDbg(self, yesNo):
-        self.commandDebug = yesNo
+    def cmdDbg(self, yesNo):
+        self.cmdDebug = yesNo
 
     def moveDbg(self, yesNo):
         self.moveDebug = yesNo
 
-    def movePrt(self, yesNo):
-        self.printMove = yesNo
+    def cmdPrt(self, yesNo):
+        self.printCmd = yesNo
 
-    def movePrint(self, moveStr):
-        if self.printMove:
-            print(moveStr)
-
+    def cmdPrint(self, cmdStr):
+        if self.printCmd:
+            print(cmdStr)
